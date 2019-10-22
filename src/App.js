48f4de85726ca4195b7282c3259fca94434a1724
Stewart Cintra <stewartcintra@hotmail.com>
2019-10-22 17:42:33 -0300

    adicionando repositórios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Word</h1>
-    </div>
-  );
+    return (
+        <>
+            <Routes />
+            <GlobalStyle />
+        </>
+    );
 }
 
 export default App;
