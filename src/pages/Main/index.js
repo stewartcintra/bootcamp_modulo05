36ad51b2ac9bf92e7abb53af0dc0753d7bdf741a
Stewart Cintra <stewartcintra@hotmail.com>
2019-10-23 17:52:02 -0300

    falntando 2 aulas, segunda iniciada

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-import { Container, Form, SubmitButton } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 import api from '../../services/api';
 
 export default class Main extends Component {
@@ -10,6 +12,23 @@ export default class Main extends Component {
         repositories: [],
         loading: false,
     };
+
+    componentDidMount() {
+        const repositories = localStorage.getItem('repositories')
+
+        if (repositories) {
+            this.setState({ repositories: JSON.parse(repositories) });
+        }
+
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { repositories } = this.state;
+
+        if (prevState.respositories !== repositories) {
+            localStorage.setItem('repositories', JSON.stringify(repositories));
+        }
+    }
 
     handleInputChange = e => {
         this.setState({ newRepo: e.target.value })
@@ -37,7 +56,7 @@ export default class Main extends Component {
 
 
     render() {
-        const { newRepo, loading } = this.state;
+        const { newRepo, repositories, loading } = this.state;
 
         return (
             <Container>
@@ -56,6 +75,15 @@ export default class Main extends Component {
                         { loading ? <FaSpinner color="#fff" size={14} /> : <FaPlus color="#fff" size={14} /> }
                     </SubmitButton>
                 </Form>
+
+                <List>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <span>{repository.name}</span>
+                            <Link to={`repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
+                        </li>
+                    ))}
+                </List>
             </Container>
         );
     }
